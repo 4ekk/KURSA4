da9642e392f80701d06cd3ff8e267b1d73f9fed1
4ekk <162356331+4ekk@users.noreply.github.com>
2024-05-14 23:56:13 +0500

    Add files via upload

--- Практ. работа/main.js
+++ Практ. работа/main.js
@@ -1,7 +1,7 @@
 var images = [
+    "url('https://celes.club/uploads/posts/2022-01/1641823376_68-celes-club-p-ozero-kaindi-priroda-krasivo-foto-77.jpg')",
     "url('https://gtravel.kz/wp-content/uploads/2019/07/kanyeny_2_1.jpg')",
-    "url('https://kokshetau.online/wp-content/uploads/2018/08/Progulka-po-Golubomu-zalivu-Borovoe-P1250381.jpg')",
-    "url('https://img.goodfon.ru/original/1920x1080/4/a9/ozero-kaindy-utonuvshiy-les.jpg')"
+    "url('https://kokshetau.online/wp-content/uploads/2018/08/Progulka-po-Golubomu-zalivu-Borovoe-P1250381.jpg')"
 ];
 var currentImageIndex = 0;
 var imageContainer = document.querySelector('.image-container');
@@ -12,23 +12,4 @@ function changeImage() {
         currentImageIndex = 0;
     }
     imageContainer.style.backgroundImage = images[currentImageIndex];
-    imageContainer.style.filter = "brightness(0.7)";
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    changeImage();
-});
-
-var button = document.querySelector('.button');
-button.addEventListener('click', function() {
-    changeImage();
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    var listElement = document.querySelector('.list');
-    var miniWindow = document.querySelector('.mini-window');
-    
-    listElement.addEventListener('click', function() {
-        miniWindow.classList.toggle('show');
-    });
-});
